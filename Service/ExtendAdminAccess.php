4a1048b939117f51a2f9c01775f4ef34831d0e0e
Rudy Masson <rmasson.pro@gmail.com>
2020-12-31 11:32:58 +0100

    fix extendAdminAccess

--- Service/ExtendAdminAccess.php
+++ Service/ExtendAdminAccess.php
@@ -4,6 +4,7 @@ namespace Akyos\FormBundle\Service;
 use Akyos\CoreBundle\Entity\AdminAccess;
 use Akyos\CoreBundle\Repository\AdminAccessRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
 
 class ExtendAdminAccess
 {
@@ -40,5 +41,7 @@ class ExtendAdminAccess
             $this->entityManager->persist($adminAccess);
             $this->entityManager->flush();
         }
+        return new Response('true');
+
     }
 }
