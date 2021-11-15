a20b94647648b147cdd4941acc6582b87e7c58e6
JOULKVA Johan <johan.akyos@gmail.com>
2021-11-15 17:34:54 +0100

    fix: isGranted

--- Controller/ContactFormSubmissionController.php
+++ Controller/ContactFormSubmissionController.php
@@ -14,6 +14,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 /**
  * @Route("/admin/contact-form-submission", name="contact_form_submission_")
--- Controller/FormController.php
+++ Controller/FormController.php
@@ -14,6 +14,7 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 
 /**
  * @Route("/admin/contact-form", name="contact_form_")
